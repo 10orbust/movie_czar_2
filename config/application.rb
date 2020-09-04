48a34c1f0f551f91afa24b4b366ea445275e4cc4
Patrick McKernin <patrick@firstdraft.com>
2020-09-04 00:19:32 -0500

    wip

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module MovieCzar
     # Initialize configuration defaults for originally generated Rails version.
     config.assets.initialize_on_precompile = false
     config.load_defaults 6.0
+    require 'dotenv'
+    Dotenv.load('file1.env')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
